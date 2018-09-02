956f904ba055d18982e99e6222502f8d5739614e
Ramon Geronimo <rgero215@gmail.com>
2018-09-02 08:37:13 -0700

    finish madlibs

---
+++ madlibs.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import webbrowser
+
+def cute_poppy(prompt):
+    word = raw_input(prompt)
+    return word
+
+def silly_word(prompt):
+    word = raw_input(prompt)
+    return word
+
+def adjetive(prompt):
+    word = raw_input(prompt)
+    return word
+
+def silly_word(prompt):
+    word = raw_input(prompt)
+    return word
+
+def plural_type_of_profession(prompt):
+    word = raw_input(prompt)
+    return word
+
+def type_of_80s_technology(prompt):
+    word = raw_input(prompt)
+    return word
+
+def something_you_buy_on_ebay(prompt):
+    word = raw_input(prompt)
+    return word
+
+def spider(prompt):
+    word = raw_input(prompt)
+    return word
+
+def type_of_relative(prompt):
+    word = raw_input(prompt)
+    return word
+
+def woman_name(prompt):
+    word = raw_input(prompt)
+    return word
+
+def number(prompt):
+    word = raw_input(prompt)
+    return word
+
+def plural_object(prompt):
+    word = raw_input(prompt)
+    return word
+
+def curse(prompt):
+    word = raw_input(prompt)
+    return word
+
+def fictional_character(prompt):
+    word = raw_input(prompt)
+    return word
+
+def something_you_find_in_the_kitchen(prompt):
+    word = raw_input(prompt)
+    return word
+
+def another_number(prompt):
+    word = raw_input(prompt)
+    return word
+
+def celebrity_name(prompt):
+    word = raw_input(prompt)
+    return word
+
+def ing_verb(prompt):
+    word = raw_input(prompt)
+    return word
+
+def another_adjetive(prompt):
+    word = raw_input(prompt)
+    return word
+
+def plural_animal(prompt):
+    word = raw_input(prompt)
+    return word
+
+def another_ing_verb(prompt):
+    word = raw_input(prompt)
+    return word
+
+def favorite_song_lyric(prompt):
+    word = raw_input(prompt)
+    return word
+
+cute_poppy_value = cute_poppy("Please enter the name of a cute poppy: ")
+silly_word_value = silly_word("Please enter a silly word: ")
+adjetive_value = adjetive("Please enter an adjetive: ")
+plural_type_of_profession_value = plural_type_of_profession("Please enter a plural type of profession: ")
+type_of_80s_technology_value = type_of_80s_technology("Please enter a type of 80's technology: ")
+something_you_buy_on_ebay_value = something_you_buy_on_ebay("Please enter something you buy on ebay: ")
+spider_value = spider("What would you shout if you saw a spider crawling up you leg: ")
+type_of_relative_value = type_of_relative("Please enter a type of relative: ")
+woman_name_value = woman_name("Please enter a woman's name: ")
+number_value = number("Please enter a number: ")
+plural_object_value = plural_object("Please enter a plural object: ")
+curse_value = curse("Please enter a curse word a first-grader would use: ")
+fictional_character_value = fictional_character("Please enter a fictional character: ")
+something_you_find_in_the_kitchen_value = something_you_find_in_the_kitchen("PLease enter something you find in the kitchen: ")
+another_number_value = another_number("Give me another number: ")
+celebrity_name_value = celebrity_name("Please enter a celebrity name: ")
+ing_verb_value = ing_verb("Please enter a verb ending in ing: ")
+another_adjetive_value = another_adjetive("Please enter another adjetive: ")
+plural_animal_value = plural_animal("Please enter a plural animal: ")
+another_ing_verb_value = another_ing_verb("Please enter another verb ending in ing: ")
+favorite_song_lyric_value = favorite_song_lyric("Please enter your favorite song lyric: ")
+
+print("– Good evening my {}.").format(cute_poppy_value)
+print("– Special Director {}. It's good to see you again.").format(silly_word_value)
+print("– I'm here with your next mission. A criminal organization with the most {} {} hacked into the government's {}, stealing their top-secret report of {}").format(adjetive_value, plural_type_of_profession_value, type_of_80s_technology_value, something_you_buy_on_ebay_value)
+print("– {}").format(spider_value)
+print("– I understand your reaction. The group also kidnapped your {} {}, and is holding her for a ransom of {} {}").format(type_of_relative_value, woman_name_value, number_value, plural_object_value)
+print("– Where the {} are we going to get {} {}?").format(curse_value, number_value, plural_object_value)
+print("– I don't know but we need to move fast.")
+print("– I have a plan. I'll meetup with the group disguissing myself as {}").format(fictional_character_value)
+print("– So far, I'm on board")
+print("– If they attack, I'll fight back with {}").format(something_you_find_in_the_kitchen_value)
+print("– Great, this disk contains sensitive information that might help.")
+print("– What's on this disk?")
+print("– {} photos of {} {}").format(another_number_value, celebrity_name_value, ing_verb_value)
+print("– {}, I already contacted my team of highly {} {} to help.").format(spider_value, another_adjetive_value, plural_animal_value)
+print("– You're the best agent we have. How do you prepare for such tense mission?")
+print("– By waking up every morning, going up of the roof, and {} and by repeating my own personal mantra {} {}").format(another_ing_verb_value, favorite_song_lyric_value, favorite_song_lyric_value)
+
+
+time.sleep(60)
+webbrowser.open_new("https://youtu.be/6iClgRjmTvc")
+
+
+
+
+
+
+
+# user_place = place("Where do you like to go? ")
+# print("{} went to {} for the first time.".format(user_name, user_place))
