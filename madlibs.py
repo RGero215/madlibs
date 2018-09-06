984e9dee7aff05912f16279a2c6d93d29ad6f0ca
Ramon Geronimo <rgero215@gmail.com>
2018-09-05 17:08:17 -0700

    add letter by letter effect

--- madlibs.py
+++ madlibs.py
@@ -3,6 +3,14 @@
 
 import time
 import webbrowser
+import sys
+
+def delay_print(string):
+    for c in string:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    print('')
 
 
 
@@ -117,25 +125,26 @@ plural_animal_value = plural_animal("Please enter a plural animal: ")
 another_ing_verb_value = another_ing_verb("Please enter another verb ending in ing: ")
 favorite_song_lyric_value = favorite_song_lyric("Please enter your favorite song lyric: ")
 
-print("– Good evening my {}.").format(cute_puppy_value)
-print("– Special Director {}. It's good to see you again.").format(silly_word_value)
-print("– I'm here with your next mission. A criminal organization with the most {} {} hacked into the government's {}, stealing their top-secret report of {}").format(adjetive_value, plural_type_of_profession_value, type_of_80s_technology_value, something_you_buy_on_ebay_value)
-print("– {}").format(spider_value)
-print("– I understand your reaction. The group also kidnapped your {} {}, and is holding her for a ransom of {} {}").format(type_of_relative_value, woman_name_value, number_value, plural_object_value)
-print("– Where the {} are we going to get {} {}?").format(curse_value, number_value, plural_object_value)
-print("– I don't know but we need to move fast.")
-print("– I have a plan. I'll meetup with the group disguissing myself as {}").format(fictional_character_value)
-print("– So far, I'm on board")
-print("– If they attack, I'll fight back with {}").format(something_you_find_in_the_kitchen_value)
-print("– Great, this disk contains sensitive information that might help.")
-print("– What's on this disk?")
-print("– {} photos of {} {}").format(another_number_value, celebrity_name_value, ing_verb_value)
-print("– {}, I already contacted my team of highly {} {} to help.").format(spider_value, another_adjetive_value, plural_animal_value)
-print("– You're the best agent we have. How do you prepare for such tense mission?")
-print("– By waking up every morning, going up of the roof, and {} and by repeating my own personal mantra {} {}").format(another_ing_verb_value, favorite_song_lyric_value, favorite_song_lyric_value)
+delay_print("– Good evening my " + cute_puppy_value)
+delay_print("– Special Director " + silly_word_value + ". It's good to see you again.")
+delay_print("– I'm here with your next mission. A criminal organization with the most " + adjetive_value +  " " + plural_type_of_profession_value +" hacked into the government's "+ type_of_80s_technology_value +", stealing their top-secret report of " + something_you_buy_on_ebay_value )
+delay_print("– "+ spider_value)
+delay_print("– I understand your reaction. The group also kidnapped your "+ type_of_relative_value + ", " + woman_name_value + " and is holding her for a ransom of " + number_value + plural_object_value)
+delay_print("– Where the " + curse_value + " are we going to get " + number_value + plural_object_value + "?")
+delay_print("– I don't know but we need to move fast.")
+delay_print("– I have a plan. I'll meetup with the group disguissing myself as " + fictional_character_value)
+delay_print("– So far, I'm on board")
+delay_print("– If they attack, I'll fight back with " + something_you_find_in_the_kitchen_value)
+delay_print("– Great, this disk contains sensitive information that might help.")
+delay_print("– What's on this disk?")
+delay_print("– " + another_number_value + " photos of " + celebrity_name_value + ing_verb_value)
+delay_print("– " + spider_value + ", I already contacted my team of highly " + another_adjetive_value +  plural_animal_value + " to help.")
+delay_print("– You're the best agent we have. How do you prepare for such tense mission?")
+delay_print("– By waking up every morning, going up of the roof, and " + another_ing_verb_value + " and by repeating my own personal mantra " + favorite_song_lyric_value + favorite_song_lyric_value )
 
 
 time.sleep(90)
+
 print("This message will auto destroy in 5 seconds")
 countdown = 5
 while countdown:
